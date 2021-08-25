9c9625fdb86fee36699adc22c1720f1d7dbd04a1
Ian Ibbotson <ianibbo@gmail.com>
2021-08-25 10:17:46 +0100

    Import code for generating package json

--- laser/ProcessLaserSubscription.groovy
+++ laser/ProcessLaserSubscription.groovy
@@ -26,4 +26,99 @@ public class ProcessLaserSubscription implements TransformProcess {
     ]
   }
 
+  private Map generateFOLIOPackageJSON(String generated_package_name, String generated_file_name, Map subscription) {
+    def pkg_data = [
+      "header": [
+         "dataSchema": [
+           "name": "mod-agreements-package",
+           "version": "1.0"
+         ],
+         "trustedSourceTI": "true"
+       ],
+       "records": [
+       ]
+    ]
+
+
+    // We build a "custom package" to describe the titles controlled by this subscription - because we want the FOLIO
+    // agreement to only contain a package rather than indvidual entitlements (WHich would be much closer to the LAS:eR model)
+    def content_items = [];
+
+    // Add a record foe the package
+    pkg_data.records.add([
+      "source": "LAS:eR",
+      "reference": subscription.globalUID,
+      "name": generated_package_name,
+      // "packageProvider": [
+      //   "name": "provider"
+      // ],
+      "contentItems": content_items
+    ])
+
+    // For each package (So far only ever seen one, but hey)
+    subscription.packages.each { pkg ->
+      // Iterate over the titles in the package and add a content item for each one
+      pkg.issueEntitlements.each { ie ->
+
+      ArrayList coverage = buildCoverageStatements(ie.coverages);
+
+        content_items.add([
+          //"depth": null,
+          "accessStart": dealWithLaserDate(ie.accessStartDate),
+          "accessEnd": dealWithLaserDate(ie.accessEndDate),
+          "coverage": coverage,
+          "platformTitleInstance": [
+            "platform": ie.tipp.platform.name,
+            "platform_url": ie.tipp.platform.primaryUrl,
+            "url": ie.tipp.hostPlatformURL,
+            "titleInstance": [
+              "name": ie.tipp.title.title,
+              "identifiers":ie.tipp.title.identifiers,
+              "type": ie.tipp.title.medium,
+              "subtype": "electronic",
+            ]
+          ]
+        ])
+      }
+    }
+
+    // String pkg_json = JsonOutput.toJson(pkg_data);
+
+    return pkg_data;
+  }
+
+  private ArrayList buildCoverageStatements(ArrayList ieCoverages) {
+    ArrayList coverageStatements = []
+    ieCoverages.each{ ieCoverage ->
+
+      def startDate = dealWithLaserDate(ieCoverage.startDate)
+      def endDate = dealWithLaserDate(ieCoverage.endDate)
+      
+      Map coverageStatement = [
+        startDate: startDate,
+        endDate: endDate,
+        startVolume: ieCoverage.startVolume,
+        endVolume: ieCoverage.endVolume,
+        startIssue: ieCoverage.startIssue,
+        endIssue: ieCoverage.endIssue
+      ]
+      coverageStatements << coverageStatement
+    }
+
+    return coverageStatements;
+  }
+
+  private String dealWithLaserDate(String laserDate) {
+    String dateOutput = null
+    if (laserDate != null) {
+      try {
+        LocalDateTime laserDateLocalDateTime = LocalDateTime.parse(laserDate)
+        LocalDate laserDateLocalDate = laserDateLocalDateTime.toLocalDate()
+        dateOutput = laserDateLocalDate.toString()
+      } catch (Exception e) {
+        println("Warning: failure parsing LAS:eR Date ${laserDate}: ${e.message}")
+      }
+    }
+    return dateOutput;
+  }
 }
