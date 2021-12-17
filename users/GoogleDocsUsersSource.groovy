265da87960306db7bc461d1eef88036bae101315
Ian Ibbotson <ianibbo@gmail.com>
2021-12-17 16:04:34 +0000

    working on users import

--- users/GoogleDocsUsersSource.groovy
+++ users/GoogleDocsUsersSource.groovy
@@ -11,26 +11,29 @@ public class GoogleDocsUsersSource implements RemoteSyncActivity {
                            Map state,
                            RecordSourceController rsc) {
 
-    String identifierType=rsc.getAppSetting('usertools.url')
+    String users_url = rsc.getAppSetting('usertools.url')
 
-    def test_records = [
-    ]
+    if ( users_url != null ) {
 
-    test_records.each { Map testrec ->
+      def test_records = [
+      ]
 
-       def license_json = JsonOutput.toJson(testrec);
-       MessageDigest md5_digest = MessageDigest.getInstance("MD5");
-       byte[] license_json_bytes = license_json.toString().getBytes()
-       md5_digest.update(license_json_bytes);
-       byte[] md5sum = md5_digest.digest();
-       String license_hash = new BigInteger(1, md5sum).toString(16);
+      test_records.each { Map testrec ->
 
-       rsc.upsertSourceRecord(source_id,
-                              'TEST',
-                              'TEST:LICENSE:'+testrec.id,
-                              'TEST:LICENSE',
-                              license_hash,
-                              license_json_bytes);
+         def license_json = JsonOutput.toJson(testrec);
+         MessageDigest md5_digest = MessageDigest.getInstance("MD5");
+         byte[] license_json_bytes = license_json.toString().getBytes()
+         md5_digest.update(license_json_bytes);
+         byte[] md5sum = md5_digest.digest();
+         String license_hash = new BigInteger(1, md5sum).toString(16);
+
+         rsc.upsertSourceRecord(source_id,
+                                'TEST',
+                                'TEST:LICENSE:'+testrec.id,
+                                'TEST:LICENSE',
+                                license_hash,
+                                license_json_bytes);
+      }
     }
 
   }
